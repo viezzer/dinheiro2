5da3cbec8611206c486bc8c348689ddd032c7058
eduviezzer <33733608+viezzer@users.noreply.github.com>
2023-12-02 11:04:10 -0300

    gh pages

--- src/components/IncomeAndExpenseDisplay.tsx
+++ src/components/IncomeAndExpenseDisplay.tsx
@@ -7,12 +7,12 @@ interface IncomeAndExpenseDisplayProps {
 
 function IncomeAndExpenseDisplay({expense, income}: IncomeAndExpenseDisplayProps) {
     return(
-        <div className="bg-card text-black flex flex-col sm:flex-row justify-evenly items-center rounded-lg py-2 px-4 w-3/4 sm:w-2/3">
-            <div className="w-1/2 border-b border-1 border-black sm:border-r sm:border-b-0">
+        <div className="bg-card text-black flex flex-col sm:flex-row justify-evenly items-center rounded-lg py-2  w-3/4 sm:w-2/3">
+            <div className="w-full border-b border-1 border-black sm:border-r sm:border-b-0 p-2">
                 <p className="font-bold text-xl">RECEITAS</p>
                 <p className="font-bold text-income text-md">R$ {income}</p>
             </div>
-            <div className="w-1/2">
+            <div className="w-full p-2">
                 <p className="font-bold text-xl">DESPESAS</p>
                 <p className="font-bold text-expense text-md">R$ {expense}</p>
             </div>
