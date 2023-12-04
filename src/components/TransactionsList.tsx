5bd2abf7befe30683b021010acb1be24022dbd74
eduviezzer <33733608+viezzer@users.noreply.github.com>
2023-12-04 20:34:02 -0300

    responsividade

--- src/components/TransactionsList.tsx
+++ src/components/TransactionsList.tsx
@@ -34,7 +34,7 @@ function TransactionsList({transactions, handleDelete}: TransactionsListProps) {
     return (
         <div className={styles.container}>
             {transactions ? (transactions.map((transaction, index) => (
-                    <div className={styles.listItemRectangle} key={index} onClick={() => {handleDelete(transaction.id, transaction.title)}}>
+                    <div className='w-full h-11 bg-card flex flex-row rounded-lg mb-2 justify-between sm:w-96 md:w-3/4' key={index} onClick={() => {handleDelete(transaction.id, transaction.title)}}>
                         <div className={styles.titleDiv}>
                             <p className={styles.title}>{transaction.title}</p>
                         </div>
